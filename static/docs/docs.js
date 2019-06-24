475910749edd5f0c42ee6316887397c7617abed6
Zirpoo <loik77@live.ca>
2019-06-23 23:47:51 -0400

    linter fix

--- static/docs/docs.js
+++ static/docs/docs.js
@@ -1,23 +1,22 @@
 window.$docsify = {
-    basePath: "/",
-    name: "difys/documentation",
-    repo: "https://github.com/AlaaZorkane/Difys",
-    subMaxLevel: 2,
-    notFoundPage: "404.md",
-    loadSidebar: true,
-    alias: {
-        "/.*/_sidebar.md": "/_sidebar.md"
-    },
-    search: {
-        maxAge: 86400000,
-        paths: [],
-        placeholder: "Type to search",
-        noData: "No Results! Maybe try discord?",
-        hideOtherSidebarContent: false
-    },
-    homepage: "getting-started/quick-start.md",
-    themeable: {
-        readyTransition: true,
-        responsiveTables: true
-    }
+	name: "difys/documentation",
+	repo: "https://github.com/AlaaZorkane/Difys",
+	subMaxLevel: 2,
+	notFoundPage: "404.md",
+	loadSidebar: true,
+	alias: {
+		"/.*/_sidebar.md": "/_sidebar.md"
+	},
+	search: {
+		maxAge: 86400000,
+		paths: [],
+		placeholder: "Type to search",
+		noData: "No Results! Maybe try discord?",
+		hideOtherSidebarContent: false
+	},
+	homepage: "getting-started/quick-start.md",
+	themeable: {
+		readyTransition: true,
+		responsiveTables: true
+	}
 }
